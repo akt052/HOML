8e9d12b9e901998660c1201976a108272af966ef
akt052 <akshat0akt52@gmail.com>
2026-07-02 11:36:16 +0530

    use knn

--- knn.py
+++ knn.py
@@ -8,14 +8,14 @@ class KNN:
         self.X_train = X
         self.Y_train = Y
 
-    def dist_metric(self, x1, x2):
-        return np.sqrt(np.sum(x1-x2)**2)
+    def eculidian_distance(self, x1, x2):
+        return np.sqrt(np.sum((x1-x2)**2))
     
 
     def predict(self, X):
 
-            x = np.atleast_2d(x)
-            predictions = []
+        X = np.atleast_2d(X)
+        predictions = []
 
         for test_point in X:
 
@@ -37,6 +37,13 @@ class KNN:
         return predictions
 
 
+# using KNN 
 
+X_train = np.array([[1,2],[2,3],[3,3],[6,7]])
+y_train = np.array([0,0,0,1])
 
-    
+knn = KNN(K=3, X=X_train, Y=y_train)
+
+X_test = np.array([[2,2.5],[5,6]])
+
+print(knn.predict(X_test))
